8dd9389c2667e49615928fd80f66902cfabde7f5
BavleyA <paula3del@gmail.com>
2024-11-11 15:20:40 +0200

    Initial commit

---
+++ lib/face_detection.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FaceDetectionScreen extends StatefulWidget {
+  const FaceDetectionScreen({super.key});
+
+  @override
+  State<FaceDetectionScreen> createState() => _FaceDetectionScreenState();
+}
+
+class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
