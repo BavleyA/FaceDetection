b47d1bc6b241419d000e02fe1fb9b31722e57367
BavleyA <paula3del@gmail.com>
2024-11-11 15:27:34 +0200

    add initial root

--- lib/face_detection.dart
+++ lib/face_detection.dart
@@ -11,7 +11,30 @@ class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Face Detection',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 250,
+                color: Colors.grey,
+                child: Center(child: Icon(Icons.add_a_photo, size: 60,)),
+              ),
+              SizedBox(height: 10,),
 
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
